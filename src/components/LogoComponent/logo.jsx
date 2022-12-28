973a23701a00f37e1b3e3fb09866de0ebf0271ca
ggospodinov <baigrozdan99@abv.bg>
2022-12-28 23:06:35 +0200

    add logocomponent

---
+++ src/components/LogoComponent/logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import image from "../../../public/images/logo.svg"
+
+
+export default function LogoCreate(){
+    return(
+    <div class="logo-create">
+        <img src={image} width="100" height="50"/>
+    </div>
+    )
+}
--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -3,11 +3,13 @@ import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
+import LogoCreate from "../LogoComponent/logo";
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
       <Container className={classNames(styles.container)} maxWidth="xl">
+          <LogoCreate/>
         <Paper className={classNames(styles.paper)}>
           <Link href="/about" color="secondary">
             Hi
