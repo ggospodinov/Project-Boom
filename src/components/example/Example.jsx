09302b4fb04b8790e158c15cf44bb211a243db0c
ggospodinov <baigrozdan99@abv.bg>
2023-01-03 20:32:27 +0200

    change logo

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -4,16 +4,20 @@ import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
 import LogoCreate from "../LogoComponent/Logo.jsx";
+// import LogoCreate1 from "../LogoComponent/Logo.jsx";
+
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
       <Container className={classNames(styles.container)} maxWidth="xl">
         <Paper className={classNames(styles.paper)}>
-          <LogoCreate/>
+          <LogoCreate />
+          <LogoCreate />
+         
           {/* <Link href="/about" color="secondary">
             Hi
-          </Link> */}
+          </Link>  */}
         </Paper>
       </Container>
     </div>
