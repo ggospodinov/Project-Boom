bf8412ac00a8f4316d6b49196010c8289cb22fc2
ggospodinov <baigrozdan99@abv.bg>
2023-01-10 20:38:38 +0200

    change logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
 
-const Logo =({type})=>{
-  
-    return(
-      
-      <div>{type}
-          
-      </div>
 
+export default function Logo ({type ="default"}){
+
+  const  defaultLogoimg = "../images/logo.svg";
+  const mutedLogoimg ="../images/logo-muted.svg";
   
-    )
+  if (type ==="default"){
+    return React.createElement("img", { src: defaultLogoimg }, null);
+  }
+   
+      
+      return React.createElement("img", { src: mutedLogoimg }, null);
+  
+    
 }
-export default Logo
+
 
